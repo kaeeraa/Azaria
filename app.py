af2ce87f8d951c2c6edf15a66522bfd48c923dd3
kaeeraa <kaeeraa@nebula-nook.ru>
2024-07-20 14:30:01 +0300

    Full config change, dummy site, changed config from yaml to toml, calling web from main.py

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+This module is the main entry point for the application.
+It imports the necessary dependencies and sets up the environment for the application to run.
+"""
+
+from os import environ
+import logging
+import threading
+import flask
 from flask import Flask
+import telebot
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = telebot.TeleBot(token=environ.get("KEY"))
+# Диспетчер
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+async def index():
+    """
+    Render the index.html template for the '/' route.
+
+    :return: The rendered index.html template.
+    """
+    return flask.render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    threading.Thread(target=bot.polling(True)).start()
--- main.py
+++ main.py
@@ -1,14 +1,50 @@
 #!/usr/bin/env python
+
+"""
+This module contains the main entry point of the application.
+It imports necessary modules and sets up the environment.
+"""
+
 # -*- coding: utf-8 -*-
+
+__VERSION__ = "0.0.1"
+
+import sys
+
 import shutil
-
-from loguru import logger
 from sys import stdout
-import yaml
-from sys import exit
+import tomllib
+from loguru import logger
+
+import app  # pylint: disable=import-error
 
 
-class Level:
+class Level:  # pylint: disable=too-few-public-methods
+    """
+    A class that defines the different log levels for the logger.
+
+    This class defines the log levels that can be used with the logger. Each log level
+    is associated with a string constant and a color constant. The string constants
+    are used to identify the log level in the log message. The color constants are used
+    to color the log message in the terminal.
+
+    The log levels are:
+    - TRACE: "TRACE"
+    - DEBUG: "DEBUG"
+    - INFO: "INFO"
+    - SUCCESS: "SUCCESS"
+    - WARNING: "WARNING"
+    - ERROR: "ERROR"
+
+    The associated colors are:
+    - TRACE: "<dim><white>"
+    - DEBUG: "<blue>"
+    - INFO: "<green>"
+    - SUCCESS: "<green>"
+    - WARNING: "<yellow>"
+    - ERROR: "<red>"
+    """
+
     TRACE = "TRACE"
     DEBUG = "DEBUG"
     INFO = "INFO"
@@ -23,12 +59,12 @@ class Level:
     WARNING_COLOR = "<yellow>"
     ERROR_COLOR = "<red>"
 
-    TRACE_ICON = "🔵"
-    DEBUG_ICON = "🔵"
-    INFO_ICON = "🟢"
-    SUCCESS_ICON = "🟢"
-    WARNING_ICON = "🟡"
-    ERROR_ICON = "🔴"
+    TRACE_ICON = "🔵 T"
+    DEBUG_ICON = "🔵 D"
+    INFO_ICON = "🟢 I"
+    SUCCESS_ICON = "🟢 S"
+    WARNING_ICON = "🟡 W"
+    ERROR_ICON = "🔴 E"
 
 
 # removing default logger, setting up levels and adding new loggers
@@ -42,52 +78,119 @@ logger.level(Level.ERROR, color=Level.ERROR_COLOR, icon=Level.ERROR_ICON)
 
 # setting up stdout handler
 logger.add(stdout,
-           format="<white>{time:HH:mm:ss}</white> | <level>[{level.icon}]</level> | <level>{message}</level>",
-           colorize=True, level="INFO")
+           format="<white>{time:HH:mm:ss}</white> |"
+                  " <level>[{level.icon}]</level> |"
+                  " <level>{message}</level>",
+           colorize=True, level=Level.INFO)
 
 # setting up file handler
 logger.add("logs/{time:YYYY-MM-DD}.log", format="{time:HH:mm:ss} | {level} | {message}",
-           level="TRACE", rotation="00:00", compression="zip", retention="7 days", colorize=False)
+           level=Level.TRACE, rotation="00:00", compression="zip", retention="7 days",
+           colorize=False)
 
 logger.trace("Logging started")
 
 
-def caught(exception: Exception, critical: bool = False):
+def caught(exception: Exception, critical: bool = False) -> None:
+    """
+    This function handles exceptions based on the critical flag.
+
+    :param exception: The exception that was caught.
+    :param critical: A flag indicating if the exception is critical.
+
+    :return: None
+    """
     if critical:
         logger.error("We have caught an exception: " + str(exception))
         input("Press any key to exit...")
+        sys.exit(1)
     else:
         logger.error("We have caught an exception: " + str(exception))
 
 
-def bad_config():
-    if input("Would you like to create a new config file? If your config file already exists, it will be renamed to "
-             "config.yaml.bak (y/n) ") == "y":
+def bad_config() -> None:
+    """
+    A function that handles the configuration setup, if the config is invalid or corrupted.
+    Prompts the user to create a new config file, renames the existing one if it exists,
+    and provides instructions on editing the config file.
+
+    :return: None
+    """
+
+    if input("Would you like to create a new config file?"
+             " If your config file already exists, it will be renamed to "
+             "config.toml.bak (y/n) ") == "y":
         try:
-            shutil.move("./user/config.yaml", "./user/config.yaml.bak")
-            shutil.copyfile("./resources/default_config.yaml", "./user/config.yaml")
-        except Exception as e:
-            logger.exception(e)
-        logger.success("Created new default config file, old config file renamed to config.yaml.bak")
-        logger.info("Please edit config.yaml to your liking")
-        input("Press any key to exit...")
-        exit()
+            shutil.move("user/config.toml", "./user/config.toml.bak")
+            shutil.copyfile("resources/default_config.toml", "user/config.toml")
+        except Exception as e:  # pylint: disable=broad-except
+            caught(e, True)
+        logger.success("Created new default config file,"
+                       " old config file renamed to config.toml.bak")
+        logger.info("Please edit config.toml to your liking")
     else:
         logger.error("Exiting...")
-        exit()
+        sys.exit()
 
-
-logger.catch(onerror=lambda e: caught(e))
 
 # loading config
-with open("./user/config.yaml") as stream:
+with open("user/config.toml", 'rb') as stream:
+    options: list = ['token', 'explicit-control']
+
     try:
-        config = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        logger.exception(exc)
+        config: dict = tomllib.load(stream)
+    except tomllib.TOMLDecodeError as exc:
+        caught(exc)
         bad_config()
 
     if config is None:
+        caught("Config file is empty")
+        bad_config()
+
+    if 'config-version' not in list(config.keys()):
+        caught("Config file is missing config-version")
+        bad_config()
+
+    if config['config-version'] != int(__VERSION__.replace(".", "")):
+        caught("Config version mismatch")
+        bad_config()
+
+    try:
+        if not isinstance(config['token'], str) or \
+                not isinstance(config['explicit-control']['enabled'], bool) or \
+                not isinstance(config['explicit-control']['type'], str):
+            caught("Config file is invalid")
+            bad_config()
+    except KeyError as exc:
+        caught("Config file is missing keys")
         bad_config()
 
     logger.info("Config loaded")
+
+if config['explicit-control']['enabled']:
+    logger.info("Explicit control enabled")
+    match config['explicit-control']['type'].lower():
+        case "web":
+            logger.info("Running in web mode")
+            panelType: str = "web"
+        case "cli":
+            logger.info("Running in cli (terminal) mode")
+            panelType: str = "cli"
+        case _:
+            logger.warning("Invalid explicit control mode, using cli (terminal) mode")
+            logger.info("Running in cli (terminal) mode")
+            panelType: str = "cli"
+else:
+    logger.info("Explicit control disabled")
+    while 1:
+        panelType = input("Enter 'web' for web mode, 'cli' for cli (terminal) mode: ").lower()
+        if panelType in ["web", "cli"]:
+            break
+        logger.warning("Invalid input, please enter 'web' or 'cli'")
+    logger.info(f"Running in {panelType} mode")
+
+    app.app.run(use_reloader=False, host="0.0.0.0", port=7171, threaded=True)
+
+    logger.info("More is not implemented yet (indev)")
+
+    # raise NotImplementedError
